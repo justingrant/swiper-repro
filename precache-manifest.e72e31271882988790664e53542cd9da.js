8e2a76c36a510a30fa14843bc323726a18169c78
Justin Grant <justin@justingrant.net>
2019-10-21 18:43:20 -0700

    Updates

---
+++ precache-manifest.e72e31271882988790664e53542cd9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13797bc54e637cdbfd55a8053febf7bb",
+    "url": "/swiper-repro/index.html"
+  },
+  {
+    "revision": "acdfefb39958bb7c0e9a",
+    "url": "/swiper-repro/static/css/2.3f358699.chunk.css"
+  },
+  {
+    "revision": "7c64a4329843a5d1a6d7",
+    "url": "/swiper-repro/static/css/main.d92272cb.chunk.css"
+  },
+  {
+    "revision": "acdfefb39958bb7c0e9a",
+    "url": "/swiper-repro/static/js/2.bfaa1fcd.chunk.js"
+  },
+  {
+    "revision": "7c64a4329843a5d1a6d7",
+    "url": "/swiper-repro/static/js/main.41485841.chunk.js"
+  },
+  {
+    "revision": "f5cf15c4cdfe1ee9c5ec",
+    "url": "/swiper-repro/static/js/runtime-main.2ba3998f.js"
+  }
+]);
