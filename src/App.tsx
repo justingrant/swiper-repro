19e8d50c7ffc6da15f49518896584a224c8aafee
Justin Grant <justin@justingrant.net>
2019-10-21 18:22:05 -0700

    Add Swiper demo to create-react-app boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useLayoutEffect } from 'react';
+import Swiper from 'swiper';
+import 'swiper/css/swiper.css';
 
 const App: React.FC = () => {
+  useLayoutEffect(() => {
+    /* const swiper = */ new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      slidesPerView: 1,
+      initialSlide: 50,
+      mousewheel: {
+        forceToAxis: true,
+        invert: true,
+      },
+      freeMode: true,
+      freeModeSticky: true,
+      freeModeMomentumRatio: 0.5,
+      virtual: {
+        slides: Array.from(new Array(100).keys(), i => `Slide #${i}`),
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="swiper-container">
+      <div className="swiper-wrapper" />
     </div>
   );
-}
+};
 
 export default App;
